b2fa5666ad28560839b1b1678ba6a00b0bcc8353
R. Ashwin <Ashwin3104@gmail.com>
2025-06-21 12:09:13 +0530

    Add files via upload

---
+++ IndexError.py
@@ -0,0 +1,10 @@
+L=[10,20,30,40,50]
+
+try:
+    index=9
+    print[L[index]]
+
+except:
+    print('Invalid index')
+
+    print('End of program')
---
+++ ZeroValueError.py
@@ -0,0 +1,8 @@
+a,b=10,0
+
+try:
+    c=a//b
+    print(c)
+except:
+    print('B should not be zero')
+print('End of program')
